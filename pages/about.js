a70fee98e3cabda497ab85dc58685a4d9eb2638a
Royee Shemesh <royee_shemesh@intuit.com>
2019-06-06 16:33:44 +0300

    SAVEPOINT

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,32 @@
+import axios from 'axios';
 import { Component } from "react";
 import Link from "next/link";
 import Header from "../components/header";
 
 class AboutPage extends Component {
-  static getInitialProps() {
+  static async getInitialProps(appContext) {
+
+    let result;
+
+    console.info(appContext.query.id);
+
+    try {
+      const {data} = await axios.get(`https://swapi.co/api/people/${appContext.query.id || '1' }`);
+      result = data;
+
+    } catch (e) {
+      console.info(e);
+    }
+
+
+
     const isServer = typeof window === "undefined";
-    return { isServer };
+    return { isServer, result };
   }
 
   render() {
+    const {result} = this.props;
+
     return (
       <main>
         <Header />
@@ -23,6 +41,15 @@ class AboutPage extends Component {
           <Link href="/">
             <a>Go to Home</a>
           </Link>
+
+          <table border="1">
+            <tbody>
+            <tr>
+              <td>name</td>
+              <td>{result.name}</td>
+            </tr>
+            </tbody>
+          </table>
         </section>
       </main>
     );
